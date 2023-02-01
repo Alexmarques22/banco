cafb1da66208f029d814a2aca838018b4dc744ec
Alexmarques22 <120022336+Alexmarques22@users.noreply.github.com>
2023-02-01 11:36:53 -0300

    Add files via upload

---
+++ Atividade010223/ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atividade010223;
+
+public class ContaCorrente
+{
+    public string nome;
+    public int agencia;
+    public string conta;
+    public double saldo;
+    
+
+   
+    public void sacar (double valor )
+    {
+    
+     this.saldo -= valor;
+
+
+
+    }
+    
+
+
+    
+
+    
+
+        
+    }
+
+
+
+
+
+
+
+
+
+    
+
+     
