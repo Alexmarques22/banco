a7d1ad89aec965440dbba7346c0816d8f9eb04fa
Alexmarques22 <120022336+Alexmarques22@users.noreply.github.com>
2023-02-02 11:57:36 -0300

    Add files via upload

--- Atividade010223/ContaCorrente.cs
+++ Atividade010223/ContaCorrente.cs
@@ -3,34 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Atividade010223;
 
 public class ContaCorrente
 {
     public string nome;
     public int agencia;
     public string conta;
-    public double saldo;
+    public double saldo, valorTrasferencia;
+
     
 
-   
-    public void sacar (double valor )
+
+    public bool Sacar(double valor)
     {
-    
-     this.saldo -= valor;
+
+        if (this.saldo >= valor)
+        {
+
+
+            this.saldo -= valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("saldo insuficiente para realizar o saque. ");
+            return false;
+
+        }
+
+
 
 
 
     }
-    
 
+        public bool Transferir(double valor,ContaCorrente contaDestino)
+    {
+        if(this.saldo >= valor)
+        {
+            valorTrasferencia = valor;
+            this.saldo -= valor;
+            contaDestino.saldo = valor;
+            return true;
 
-    
+        }
+         else
+        {
+            Console.WriteLine("não foi possivel realizar a trasferencia o saldo é insuficiente");
+            return false;
+        }
 
-    
-
+    }    
         
-    }
+
+     
+
+    
+
+
+
+
+}
 
 
 
